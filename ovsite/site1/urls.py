06d2dc8fbafd5dcda87f919fc71c57e251bbc574
kirillsergeev <kirillovk317@gmail.com>
2023-05-06 12:41:24 +0300

    123

--- ovsite/site1/urls.py
+++ ovsite/site1/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('addItem/', views.add_item),
-    path('delete/<int:id>', views.delete_item)
+    path('delete/<int:id>', views.delete_item),
+    path('edit/<int:id>', views.edit_item)
 ]
--- ovsite/site1/views.py
+++ ovsite/site1/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Task
+import time
 
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'index.html', {'tasks':tasks})
+
+def edit_item(request, id):
+    if request.method == 'GET':
+        tasks = Task.objects.filter(id=id)[0]
+        return render(request, 'EditItem.html', {'tasks':tasks})
+    elif request.method == 'POST':
+        task = Task.objects.filter(id=id)[0]
+        task.name = request.POST.get('name')
+        task.text = request.POST.get('text')
+        Task.save(task)
+        return redirect('/')
 
 def delete_item(request, id):
     Task.objects.filter(id=id).delete()
@@ -14,7 +26,7 @@ def add_item(request):
         task = Task()
         task.name = request.POST.get('name')
         task.text = request.POST.get('text')
-        task.time = 121
+        task.time = f'{time.gmtime().tm_mday}.{time.gmtime().tm_mon}'
         task.save()
         return redirect('/')
 
